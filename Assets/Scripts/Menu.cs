aee19d8bf5045be950a298583ddd88f6df373cc1
RenanLorijola <renanzago@gmail.com>
2023-10-24 20:11:34 -0300

    menu button functions

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void AbrirCredito() {
+        SceneManager.LoadScene("Credits");
+    }
+    public void AbrirLeaderboard() {
+        SceneManager.LoadScene("Leaderboard");
+    }
+    public void AbrirGame() {
+        SceneManager.LoadScene("Game");
+    }
+    public void Sair() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
